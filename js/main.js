c847ce1b2bf5f02c01ad77c269dbfab1b89a9f6a
davidetaribelli <davide.taribelli@gmail.com>
2023-04-17 15:21:03 +0200

    Completato esercizio

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const contenitore = document.querySelector(".container")
 for (let i = 1; i <= 100; i++) {
-
-    contenitore.innerHTML += `<div class="box">${i}</div>`;
-
+    if (i % 3 == 0 && i % 5 == 0) {
+        contenitore.innerHTML += `<div class="box bg_color_red">Fizz</div>`;
+    } else if (i % 5 == 0){
+        contenitore.innerHTML += `<div class="box bg_color_yellow">Buzz</div>`;
+    } else if (i % 3 == 0){
+        contenitore.innerHTML += `<div class="box bg_color_green">FizzBuzz</div>`;
+    }else{
+        contenitore.innerHTML += `<div class="box">${i}</div>`;
+    }
+        
 }
